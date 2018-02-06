5a00c319ea950a658a1e9e1f7af89fa01328faee
tcscheurer <trevor.scheurer1@gmail.com>
2018-02-06 10:44:02 -0600

    finished react-4-afternoon project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import {Link} from 'react-router-dom';
+import routes from './routes';
 export default class App extends Component {
   render() {
     return (
@@ -7,10 +8,11 @@ export default class App extends Component {
         <nav className='nav'>
           <div>WestSide University</div> 
           <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
+              <Link className='links' to="/home">Home</Link>
+              <Link className='links' to="/about">About</Link> 
           </div>
         </nav>
+        {routes}
       </div>
     )
   }
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 export default class ClassList extends Component {
   constructor() {
     super()
+    this.state = {
+      students : []
+    }
     
   }
 
+  componentDidMount(){
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+      .then(response => {
+        this.setState({
+          students : response.data
+        })
+      })
+  }
+
   render() {
+    let mapped = this.state.students.map((curr,index)=>{
+      <Link  key={index} to={`/student/${curr.id}`}><h3>{curr.first_name} {curr.last_name}</h3></Link>
+    })
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+          {mapped}
       </div>
     )
   }
